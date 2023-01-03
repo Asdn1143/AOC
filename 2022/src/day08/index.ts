ce793710d0b7d290ca4b182ea60ccd1b707362ea
Albert <81871173+Asdn1143@users.noreply.github.com>
2023-01-03 20:57:24 +0000

    finished day 8

--- 2022/src/day08/index.ts
+++ 2022/src/day08/index.ts
@@ -6,27 +6,49 @@ const parseInput = (rawInput: string) => {
                           .map(a=>(a.split('')).map(a=>Number(a))))[0]
 }
 
-const checkBorderingUnits = (a:number[][]) =>{
-  console.table(a);
+
+const treeVisibleSide = (element:number, a:number[]) =>{
+  let result = true
+  
+  let current = element
+  for (let i = 0; i < a.length; i++) {
+    const e = a[i];
+    if (current<=e)
+     result = false
+  }
+  return result
+}
+
+const treeVisibleCount = (element:number,a:number[] )  =>{
+  let result =a.findIndex(a=>a>=element)  
+  return result==-1? a.length : result+1
+}
+
+const treeVisible = (element: number, a:number[][]) =>{
+  let result:boolean[] = []
+  for (let i = 0; i < a.length; i++) {
+    const e = a[i];
+    result.push(treeVisibleSide(element,e))
+  }
+
+  return result.includes(true)
+} 
+
+const checkBorderingUnits = (a:number[][], part:number = 1) =>{
+  let [height,length] = [a.length,a[0].length]  
+  let result = part==1? height*length : 0
   
   for (let row = 1; row < a.length-1; row++) {  
     for (let col = 1; col < a[0].length-1; col++) {
     const element = a[row][col];  
-    let  covered = true
-    let [height,length] = [a.length,a[0].length]  
+    
     //N,E,S,W
     let distances:number[] = [row,a.length-col-1,a.length-row-1,col]
      
-
     let neighbors:number[][] = [[],[],[],[]]
     for (let n = 0; n < distances[0]; n++) {
-        neighbors[0].push(a[row-(n+1)][col])
-        
+        neighbors[0].push(a[row-(n+1)][col])   
     }
-    if([...neighbors[0]].){
-      covered = false
-    }
-    
     for (let e = 0; e < distances[1]; e++) {
       neighbors[1].push(a[row][col+(e+1)])
     }
@@ -36,24 +58,32 @@ const checkBorderingUnits = (a:number[][]) =>{
     for (let w = 0; w < distances[3]; w++) {
       neighbors[3].push(a[row][col-(w+1)])
     }
-
-    console.log(element,neighbors,covered);
+    // console.log(element,neighbors,neighbors.map(a=>treeVisibleCount(element,a) ).reduce((a,b)=>a*b));
+    
+    if(part==1){
+    let visibility = treeVisible(element,neighbors)
+    visibility? result:result--
+    }
+    else{ 
+    let view = neighbors.map(a=>treeVisibleCount(element,a) ).reduce((a,b)=>a*b);
+    result = view>result? view:result  
+    }
     }
   }
-  return
+  return result
 }
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
   
-  let result = checkBorderingUnits(input)
+  let result = checkBorderingUnits(input,1)
   
   return result
 }
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
-  let result = 0
+  let result = checkBorderingUnits(input,2)
 
   return result 
 }
@@ -75,12 +105,16 @@ run({
   part2: {
     tests: [
       {
-        input: ``,
-        expected: 0,
+        input: `30373
+25512
+65332
+33549
+35390`,
+        expected: 8,
       },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  // onlyTests: true,
 })
